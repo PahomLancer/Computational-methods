a8218703a1c4f7d72c0677fbff4117ddb2ce9b37
PahomLancer <mozggg1@gmail.com>
2016-03-23 17:25:23 +0700

    Оптимизировал код

--- dihat_prost_2/dihat_prost_2/dihat_prost_2.cpp
+++ dihat_prost_2/dihat_prost_2/dihat_prost_2.cpp
@@ -7,39 +7,30 @@ int main()
     double fa, fb, fc;
     double a, b, c, e;
     int i = 0;
-	printf("Function is x^3-x-3=0\n");
+	printf("Function is 3x-cos(x)-1=0\n");
     printf("Input a (left): ");
     scanf("%lf", &a);
     printf("Input b (right): ");
     scanf("%lf", &b);
     printf("Input epsilon: ");
     scanf("%lf", &e);
-    c = (b + a) / 2;//center
-    fa = a*a*a - a - 3;
-    fb = b*b*b - b - 3;
-    fc = c*c*c - c - 3;
+    c = (b+a)/2; //center
+    fa = 3*a-cos(a)-1;
+    fb = 3*b-cos(b)-1;
+    fc = 3*c-cos(c)-1;
     while (abs(fc) > e)
     {
         i++;
-        if (fa*fc<0)
-        {
-            a = a;
-            b = c;
-            c = (b + a) / 2;
-            fa = a*a*a - a - 3;
-            fb = b*b*b - b - 3;
-            fc = c*c*c - c - 3;
-        }
-        else
-        {
+        if (fa*fc>0)
             a = c;
-            b = b;
-            c = (b + a) / 2;
-            fa = a*a*a - a - 3;
-            fb = b*b*b - b - 3;
-            fc = c*c*c - c - 3;
-        }
+        else
+            b = c;
+		c = (b+a)/2;
+        fa = 3*a-cos(a)-1;
+		fb = 3*b-cos(b)-1;
+		fc = 3*c-cos(c)-1;
     }
-    printf("Iterations = %d, c = %lf", i, c);
+    printf("Dihotom\n");
+	printf("Iterations = %d, c = %lf", i, c);
     return 0;
 }
