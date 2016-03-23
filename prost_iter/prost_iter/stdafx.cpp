546a4809a210c9250385dd0e10efccf2dc35eb55
PahomLancer <mozggg1@gmail.com>
2016-03-23 18:34:45 +0700

    Метод простых итераций и правка названий других методов

---
+++ prost_iter/prost_iter/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// prost_iter.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
